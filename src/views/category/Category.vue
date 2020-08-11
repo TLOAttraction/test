95c9160191bb8c0405884d942815abab88eb66e7
SummerWind <67528950+TLOAttraction@users.noreply.github.com>
2020-08-11 16:41:57 +0800

    分类页面第二次修改

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="box">
     <h2>分类</h2>
     <h1>我是项目新增的内容</h1>
     <div class="box1"></div>
@@ -17,6 +17,9 @@
     width: 100px;
     height: 100px;
     background-color: #d62708ea;
+    
+  }
+  .box{
     color: blue;
   }
 </style>
